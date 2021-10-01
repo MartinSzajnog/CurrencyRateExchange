34a2b40564401856bc608249a6e3c3cf4ddbc4cb
SzajnogMartin <szajnogmartin@gmail.com>
2021-10-01 14:03:11 +0200

    Poprawiono do testów. Wprowdzono dodatkowo datę jako parametr w metodzie getInputStream

--- src/main/java/com/example/currencyexchangerate/service/NBPClientService.java
+++ src/main/java/com/example/currencyexchangerate/service/NBPClientService.java
@@ -3,6 +3,7 @@ package com.example.currencyexchangerate.service;
 import com.example.currencyexchangerate.model.CurrencyCode;
 import com.example.currencyexchangerate.model.ExchangeRate;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
@@ -10,51 +11,64 @@ import java.io.*;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 @Component
+@AllArgsConstructor
 public class NBPClientService {
 
+    private static final String NBP_URL = "http://api.nbp.pl/api/exchangerates/rates/c/";
 
-        private static final String NBP_URL = "http://api.nbp.pl/api/exchangerates/rates/c/";
+    public ExchangeRate getExchangeRateByCurrencyCode(@NotNull CurrencyCode currencyCode, LocalDateTime dateTime) throws IOException {
 
-        public ExchangeRate getExchangeRateByCurrencyCode(@NotNull CurrencyCode currencyCode) throws IOException {
+        InputStream is = getInputStream(currencyCode, dateTime);
 
-            URL url = new URL(NBP_URL + currencyCode.name() + "?format=json");
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            String jsonText = readAll(br);
 
-            try (InputStream is = url.openStream()) {
-                BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-                String jsonText = readAll(br);
+            JSONObject json = new JSONObject(jsonText);
 
-                JSONObject json = new JSONObject(jsonText);
+            BigDecimal bidRate = json.getJSONArray("rates").getJSONObject(0).getBigDecimal("bid");
+            BigDecimal askRate = json.getJSONArray("rates").getJSONObject(0).getBigDecimal("ask");
+            ExchangeRate exchangeRate = new ExchangeRate(bidRate, askRate);
 
-                BigDecimal bidRate = json.getJSONArray("rates").getJSONObject(0).getBigDecimal("bid");
-                BigDecimal askRate = json.getJSONArray("rates").getJSONObject(0).getBigDecimal("ask");
-                ExchangeRate exchangeRate = new ExchangeRate(bidRate, askRate);
+            // Wstępnie przeglądamy zawartość z strumienia czy dobrze zaczytuje dane walut
 
-                // Wstępnie przeglądamy zawartość z strumienia czy dobrze zaczytuje dane walut
+            System.out.println("Waluta: " + currencyCode.name());
+            System.out.println("Kurs zakupu: " + bidRate);
+            System.out.println("Kurs sprzedaży: " + askRate);
 
-                System.out.println("Waluta: " + currencyCode.name());
-                System.out.println("Kurs zakupu: " + bidRate);
-                System.out.println("Kurs sprzedaży: " + askRate);
-
-                return exchangeRate;
-            }
-            //Po wykonaniu zczytania zamykamy kran z strumieniem. Zamykamy strumień wejściowy.
-
+            return exchangeRate;
+        } finally {
+            is.close();    //Po wykonaniu zczytania zamykamy kran z strumieniem. Zamykamy strumień wejściowy.
         }
 
-        private String readAll(Reader rd) throws IOException {
-            StringBuilder sb = new StringBuilder();
-            int currentRD;
+    }
 
-            while ((currentRD = rd.read()) != -1) {
-                sb.append((char) currentRD);
-            }
-            return sb.toString();
+    public InputStream getInputStream(CurrencyCode currencyCode, LocalDateTime dateTime) throws IOException {
+        URL url;
+        if (dateTime != null) {
+            url = new URL(NBP_URL + currencyCode.name() + "/" + dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + "?format=json");
+        } else {
+            url = new URL(NBP_URL + currencyCode.name() + "?format=json");
         }
+
+        InputStream is = url.openStream();
+        return is;
+    }
+
+    private String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int currentRD;
+
+        while ((currentRD = rd.read()) != -1) {
+            sb.append((char) currentRD);
+        }
+        return sb.toString();
+    }
+
+
 }
-
-
-
-
